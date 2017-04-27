a98f2079076e79313928a98e4984dcdc79bfa9a6
Nate Bosch <nbosch@google.com>
2017-04-27 16:49:45 -0700

    Add stream_transformers library and buffer utility
    - Add stream_transformers library marked `@experimental` so we can
      iterate a bit on the APIs.
    - Add `_Buffer` implementation which can collect values from a source
      stream and only emit them when a 'trigger' stream fires.
    - Add top level method `buffer` which instantiates the `_Buffer` class.
      The stream transformers in this library will all be exposed as a top
      level method.

---
+++ lib/src/stream_transformers/buffer.dart
@@ -0,0 +1,119 @@
+// Copyright (c) 2017, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+import 'dart:async';
+
+/// Creates a [StreamTransformer] which collects values and emits when it sees a
+/// value on [trigger].
+///
+/// If there are no pending values when [trigger] emits, the next value on the
+/// source Stream will immediately flow through. Otherwise, the pending values
+/// are released when [trigger] emits.
+StreamTransformer<T, List<T>> buffer<T>(Stream trigger) =>
+    new _Buffer(trigger, _collectToList);
+
+List<T> _collectToList<T>(T element, List<T> soFar) {
+  soFar ??= <T>[];
+  soFar.add(element);
+  return soFar;
+}
+
+/// A strategy for aggregating values.
+typedef R _Collect<T, R>(T element, R soFar);
+
+/// A StreamTransformer which aggregates values and emits when it sees a value
+/// on [_trigger].
+///
+/// If there are no pending values when [_trigger] emits the first value on the
+/// source Stream will immediately flow through. Otherwise, the pending values
+/// and released when [_trigger] emits.
+class _Buffer<T, R> implements StreamTransformer<T, R> {
+  final Stream _trigger;
+  final _Collect _collect;
+
+  _Buffer(this._trigger, this._collect);
+
+  @override
+  Stream<R> bind(Stream<T> values) {
+    StreamController<R> controller;
+    if (values.isBroadcast) {
+      controller = new StreamController<R>.broadcast();
+    } else {
+      controller = new StreamController<R>();
+    }
+
+    R currentResults;
+    bool waitingForValue = false;
+    StreamSubscription valuesSub;
+    StreamSubscription triggerSub;
+
+    cancelValues() {
+      var sub = valuesSub;
+      valuesSub = null;
+      return sub?.cancel() ?? new Future.value();
+    }
+
+    cancelTrigger() {
+      var sub = triggerSub;
+      triggerSub = null;
+      return sub?.cancel() ?? new Future.value();
+    }
+
+    closeController() {
+      var ctl = controller;
+      controller = null;
+      return ctl?.close() ?? new Future.value();
+    }
+
+    controller.onListen = () {
+      emit() {
+        controller.add(currentResults);
+        currentResults = null;
+      }
+
+      valuesSub = values.listen(
+          (T value) {
+            currentResults = _collect(value, currentResults);
+            if (waitingForValue) {
+              emit();
+              waitingForValue = false;
+            }
+          },
+          onError: controller.addError,
+          onDone: () {
+            valuesSub = null;
+          });
+      triggerSub = _trigger.listen(
+          (_) {
+            if (currentResults == null) {
+              waitingForValue = true;
+              return;
+            }
+            emit();
+            if (valuesSub == null) {
+              controller.close();
+              cancelTrigger();
+            }
+          },
+          onError: controller.addError,
+          onDone: () async {
+            cancelValues();
+            closeController();
+          });
+    };
+
+    // Forward methods from listener
+    if (!values.isBroadcast) {
+      controller.onPause = () {
+        valuesSub?.pause();
+        triggerSub?.pause();
+      };
+      controller.onResume = () {
+        valuesSub?.resume();
+        triggerSub?.resume();
+      };
+    }
+    controller.onCancel = () => Future.wait([cancelValues(), cancelTrigger()]);
+    return controller.stream;
+  }
+}
---
+++ test/stream_transformers/buffer_test.dart
@@ -0,0 +1,118 @@
+import 'dart:async';
+
+import 'package:test/test.dart';
+
+import 'package:async/stream_transformers.dart';
+
+void main() {
+  test('does not emit before `trigger`', () async {
+    var trigger = new StreamController();
+    var values = new Stream.fromIterable([1, 2, 3]);
+    var hasEmitted = false;
+    values.transform(buffer(trigger.stream)).listen((_) {
+      hasEmitted = true;
+    });
+    await new Future(() {});
+    expect(hasEmitted, false);
+    trigger.add(null);
+    await new Future(() {});
+    expect(hasEmitted, true);
+  });
+
+  test('emits immediately if trigger emits before a value', () async {
+    var trigger = new StreamController();
+    var values = new StreamController();
+    trigger.add(null);
+    var hasEmitted = false;
+    values.stream.transform(buffer(trigger.stream)).listen((_) {
+      hasEmitted = true;
+    });
+    values.add(1);
+    await new Future(() {});
+    expect(hasEmitted, true);
+  });
+
+  test('cancels value subscription when output canceled', () async {
+    var trigger = new StreamController();
+    var valuesCanceled = false;
+    var values = new StreamController()
+      ..onCancel = () {
+        valuesCanceled = true;
+      };
+    var subscription =
+        values.stream.transform(buffer(trigger.stream)).listen((_) {});
+    subscription.cancel();
+    expect(valuesCanceled, true);
+  });
+
+  test('cancels trigger subscription when output canceled', () async {
+    var triggerCanceled = false;
+    var trigger = new StreamController()
+      ..onCancel = () {
+        triggerCanceled = true;
+      };
+    var subscription =
+        new Stream.empty().transform(buffer(trigger.stream)).listen((_) {});
+    subscription.cancel();
+    expect(triggerCanceled, true);
+  });
+
+  test('output stream ends when trigger ends', () async {
+    var trigger = new StreamController();
+    var isDone = false;
+    _infiniteValues().transform(buffer(trigger.stream)).listen((_) {},
+        onDone: () {
+      isDone = true;
+    });
+    trigger.close();
+
+    await new Future(() {});
+    expect(isDone, true);
+  });
+
+  test('closes when trigger closes', () async {
+    var trigger = new StreamController();
+    var outputClosed = false;
+    _infiniteValues().transform(buffer(trigger.stream)).listen((_) {},
+        onDone: () {
+      outputClosed = true;
+    });
+    await trigger.close();
+    await new Future(() {});
+    expect(outputClosed, true);
+  });
+
+  test('closes after outputting final values when source closes', () async {
+    var trigger = new StreamController();
+    var values = new StreamController();
+    var outputClosed = false;
+    values.stream.transform(buffer(trigger.stream)).listen((_) {}, onDone: () {
+      outputClosed = true;
+    });
+    values.add(1);
+    await values.close();
+    expect(outputClosed, false);
+    trigger.add(null);
+    await new Future(() {});
+    expect(outputClosed, true);
+  });
+
+  test('closes immediately if there are no pending values when source closes',
+      () async {
+    var trigger = new StreamController();
+    var values = new StreamController();
+    var outputClosed = false;
+    values.stream.transform(buffer(trigger.stream)).listen((_) {}, onDone: () {
+      outputClosed = true;
+    });
+    await values.close();
+    expect(outputClosed, true);
+  });
+}
+
+Stream _infiniteValues() async* {
+  while (true) {
+    yield '';
+    await new Future(() {});
+  }
+}
