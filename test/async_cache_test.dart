7df1ab03555c210bf66f4d3110c83094560f9ac1
Mike Fairhurst <mfairhurst@google.com>
2018-07-13 16:38:55 -0700

    Fix awaits only, making cache.invalidate return a future can be separate

--- test/async_cache_test.dart
+++ test/async_cache_test.dart
@@ -58,9 +58,9 @@ void main() {
     var timesCalled = 0;
     call() async => 'Called ${++timesCalled}';
     expect(await cache.fetch(call), 'Called 1');
-    await cache.invalidate();
+    cache.invalidate();
     expect(await cache.fetch(call), 'Called 2');
-    await cache.invalidate();
+    cache.invalidate();
     expect(await cache.fetch(call), 'Called 3');
   });
 
@@ -129,9 +129,9 @@ void main() {
     }
 
     expect(await cache.fetchStream(call).toList(), ['Called 1']);
-    await cache.invalidate();
+    cache.invalidate();
     expect(await cache.fetchStream(call).toList(), ['Called 2']);
-    await cache.invalidate();
+    cache.invalidate();
     expect(await cache.fetchStream(call).toList(), ['Called 3']);
   });
 
