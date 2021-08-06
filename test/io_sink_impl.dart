0314d86e9031a092727551204c34c7417ec85315
Lasse R.H. Nielsen <lrn@google.com>
2021-08-06 15:15:06 +0200

    Deprecate the sink_base classes.
    The approach was not considered a good fit for the package API,
    and we'll remove the classes and consider another approach, if one is needed.

--- test/io_sink_impl.dart
+++ test/io_sink_impl.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@deprecated
+library io_sink_impl;
+
 import 'dart:io';
 
 import 'package:async/async.dart';
